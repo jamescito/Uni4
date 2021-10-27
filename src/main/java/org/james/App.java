4f71134821d25cc92b2fee61356c8d3cd265ea0f
jamescito <jr76407900@gmail.com>
2021-10-27 15:16:37 -0600

    Agregamos la funcionalidad en el main o app

--- src/main/java/org/james/App.java
+++ src/main/java/org/james/App.java
@@ -3,14 +3,14 @@ package org.james;
 public class App 
 {
     public static void main( String[] args )  {
-        Empleado emp = new Empleado();
-        emp.nombre="";
-        emp.apellido="";
-        emp.cedula="";
-        emp.tipo_empleado="vendedor";
-        emp.horas_extras = 5;
-        emp.ventasM = 1000;
+        Empleado e = new Empleado();
+        e.nombre="james";
+        e.apellido="reyes";
+        e.cedula="610-2304-2323G";
+        e.tipo_empleado="vendedor";
+        e.horas_extras = 5;
+        e.ventasM = 1000;
 
-        System.out.println(emp.CalcularSalarioBruto("vendedor",2000,8));
+        System.out.println(e.CalcularSalarioBruto("vendedor",2000,8));
     }
 }
