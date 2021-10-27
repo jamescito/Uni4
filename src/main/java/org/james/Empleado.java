5d9855645562ee0c76702f537ceaa2e9e389f2df
jamescito <jr76407900@gmail.com>
2021-10-27 15:12:13 -0600

    Agregamos la funcion de lo que es validacion de salario

--- src/main/java/org/james/Empleado.java
+++ src/main/java/org/james/Empleado.java
@@ -4,9 +4,9 @@ public class Empleado  {
 
     String nombre, apellido, cedula, tipo_empleado;
     float horas_extras, ventasM, salarioBase = 0;
-    float salario_base1 = 0;
-    float salario_bruto = 0;
-    float salario_neto = 0;
+    float s_base1 = 0;
+    float s_bruto = 0;
+    float s_neto = 0;
     float salar_h = 0;
     float retencion = 0;
 
@@ -29,16 +29,37 @@ public class Empleado  {
     float venta(float ventam,float salariob,float h_e){
 
         if(ventam >= 1000 && ventasM <=1499){
-            salario_base1 = salariob + 100;
+            s_base1 = salariob + 100;
             salar_h = h_e * 20;
-            salario_bruto = salario_base1 + salar_h;
+            s_bruto = s_base1 + salar_h;
         }
         if(ventam>= 1500){
-            salario_base1 = salariob + 200;
+            s_base1 = salariob + 200;
             salar_h = h_e * 20;
-            salario_bruto = salario_base1 + salar_h;
+            s_bruto = s_base1 + salar_h;
         }
-        return salario_bruto;
+        return s_bruto;
+    }
+
+    float SalarioNeto(float salario_bruto){
+
+        if(salario_bruto < 0 ){
+            throw new RuntimeException("Este salario es menor de 0");
+        }
+        
+        if(salario_bruto < 1000){
+            s_neto = salario_bruto;
+        }
+        else if (salario_bruto >= 1000 && salario_bruto<1500){
+            retencion = (float) 0.16;
+            s_neto = salario_bruto - retencion;
+        }
+        else if (salario_bruto >= 1500){
+            retencion =(float) 0.18;
+            s_neto = salario_bruto - retencion;
+        }
+
+        return s_neto;
     }
 
 
